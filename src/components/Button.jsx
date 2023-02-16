08615c3d930d8a64afd486ca232a4c610221696d
Emmanuel <oyedelee22@gmail.com>
2023-02-16 06:12:49 +0000

    update:created new pages, and components

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Button = ({buttonColor, buttonLay, children}) => {
+
+
+    const btnColor = ['orange', 'outline']; 
+    const btnLayout = ['relative', 'absolute']
+
+
+    const checkButtonColor = btnColor.includes(buttonColor) ? buttonColor : null
+
+    const checkButtonLayout = btnLayout.includes(buttonLay) ? buttonLay : null
+
+  return (
+    <>
+
+        <button className={
+            `btn
+            ${checkButtonColor}
+            ${checkButtonLayout}` 
+        }> 
+            {children}
+        </button>
+    
+    </>
+  )
+}
+
+export default Button
---
+++ src/components/Header_hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header_hero = () => {
+  return (
+    <>
+    <div className="header_hero">
+      header
+    
+    </div>
+    
+
+    </>
+  )
+}
+
+export default Header_hero
